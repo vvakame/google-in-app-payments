a215206e22b415e02eb86251db1ccebcbb2d7cfe
vvakame <vvakame+dev@gmail.com>
2011-12-19 15:02:37 +0900

    Slim3 project 作成

---
+++ google-in-app-payments/src/test/java/net/vvakame/googleinapppayments/controller/IndexControllerTest.java
@@ -0,0 +1,45 @@
+package net.vvakame.googleinapppayments.controller;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Test;
+import org.slim3.tester.ControllerTestCase;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+
+public class IndexControllerTest extends ControllerTestCase {
+
+  @Test
+  public void test() throws NullPointerException, IllegalArgumentException,
+      IOException, ServletException {
+    tester.start("/");
+    assertThat(tester.response.getStatus(),
+        is(equalTo(HttpServletResponse.SC_OK)));
+  }
+
+  LocalServiceTestHelper helper;
+
+  @Override
+  public void setUp() throws Exception {
+    LocalDatastoreServiceTestConfig dsConfig = new LocalDatastoreServiceTestConfig();
+    dsConfig.setNoStorage(true);
+    dsConfig.setNoIndexAutoGen(true);
+    helper = new LocalServiceTestHelper(dsConfig);
+    helper.setUp();
+    super.setUp();
+  }
+
+  @Override
+  public void tearDown() throws Exception {
+    super.tearDown();
+    helper.tearDown();
+  }
+}
